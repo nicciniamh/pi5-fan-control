c83274037e0caa884c0942c97e0588c5ba3bfe81
Nicole Stevens <nic@ducksfeet.com>
2024-08-02 11:35:09 -0700

    Added checks for required paths to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 dir="$(pwd)"
 answer="none"
+ok="true"
+if [[  $(id -u) != 0 ]] ; then
+	echo "This script must be run as root" >&1
+	exit 1
+fi
+if [[ ! -f "/sys/class/thermal/cooling_device0/cur_state" ]] ; then
+	echo "Cannot find fan control interface at /sys/class/thermal/cooling_device0/cur_state" >&2
+	echo "(is the pwm_fan kernel module loaded?)" >&2
+	ok="false"
+fi
+if [[ ! -f "/sys/devices/virtual/thermal/thermal_zone0/temp" ]]; then
+	echo "Cannot find CPU temperature interface at /sys/devices/virtual/thermal/thermal_zone0/temp" >&2
+	ok="false"
+fi
+if [[ "$ok" == "false" ]] ; then
+	echo "Aborting due to errors" >&2
+	exit 1
+fi
 while [[ ${answer} != "yes" ]] ; do
 	echo -n "This script will edit, install, enable, and start the service. Proceeed? [yes|no]: "
 	read answer
